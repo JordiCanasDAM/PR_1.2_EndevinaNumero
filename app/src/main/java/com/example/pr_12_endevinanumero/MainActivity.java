32e2a9828218e50d97515b0bc58f3ce96b4e2eb2
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-09-26 17:14:01 +0200

    Components defined

--- app/src/main/java/com/example/pr_12_endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/pr_12_endevinanumero/MainActivity.java
@@ -2,11 +2,28 @@ package com.example.pr_12_endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //COMPONENTS
+        EditText guess = findViewById(R.id.editTextGuess);
+        Button check = findViewById(R.id.buttonCheck);
+
+        //BUTTON CLICK
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("INFO","El número introduït és: ");
+            }//onClick method
+        });//onClickListener
     }
 }
