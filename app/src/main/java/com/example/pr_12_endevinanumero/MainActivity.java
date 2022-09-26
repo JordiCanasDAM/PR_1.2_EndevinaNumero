1e769e3d5e197aa4a21f9b683f7ba82843d57925
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-09-26 16:58:18 +0200

    First commit

---
+++ app/src/main/java/com/example/pr_12_endevinanumero/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.pr_12_endevinanumero;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
