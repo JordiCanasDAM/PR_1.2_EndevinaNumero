d05d1fd4a9182862313cb9a60cba1fa0886ebfbc
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-10-09 20:20:18 +0200

    Finished TableLayout version

--- app/src/main/java/com/example/pr_12_endevinanumero/HighScore.java
+++ app/src/main/java/com/example/pr_12_endevinanumero/HighScore.java
@@ -2,8 +2,12 @@ package com.example.pr_12_endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class HighScore extends AppCompatActivity {
@@ -13,9 +17,45 @@ public class HighScore extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
 
-        //COMPONENTS
+        //TextView
+        /*
         TextView scores = findViewById(R.id.textView3);
         for (String[] a : MainActivity.highScores){scores.setText(a[0] + ":" + a[1]);};
+        */
 
+        //TableLayout
+        TableLayout scoreTable = findViewById(R.id.scoresTable);
+
+        TableRow row = new TableRow(this);
+
+        TextView score = new TextView(this);
+        score.setText("SCORE");
+        score.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        row.addView(score);
+
+        TextView user = new TextView(this);
+        user.setText("USERS");
+        user.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        row.addView(user);
+
+        scoreTable.addView(row);
+
+        for (String[] a : MainActivity.highScores){
+
+            row = new TableRow(this);
+
+            user = new TextView(this);
+            user.setText(a[0]);
+            user.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            row.addView(user);
+
+            score = new TextView(this);
+            score.setText(a[1]);
+            score.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            row.addView(score);
+
+            scoreTable.addView(row);
+
+        }
     }
 }
